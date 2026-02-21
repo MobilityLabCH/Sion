803168f26d60aa56e96c2a0876abd8e0242f1652
MobilityLabCH <ericimstepf@gmail.com>
2026-02-21 21:59:30 +0100

    Update Results.tsx

--- apps/web/src/pages/Results.tsx
+++ apps/web/src/pages/Results.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useApp } from '../hooks/store.ts';
+import { useApp } from '../hooks/store';
 import ZoneMap from '../components/ZoneMap.tsx';
 import KPICard from '../components/KPICard.tsx';
 import CategoryPill from '../components/CategoryPill.tsx';
