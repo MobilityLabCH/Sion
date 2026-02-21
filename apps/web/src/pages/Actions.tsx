08836a790f23f8176e1ec46aff08e8aebbf1364d
MobilityLabCH <ericimstepf@gmail.com>
2026-02-21 21:59:50 +0100

    Update Actions.tsx

--- apps/web/src/pages/Actions.tsx
+++ apps/web/src/pages/Actions.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useApp } from '../hooks/store.ts';
+import { useApp } from '../hooks/store';
 import * as api from '../lib/api.ts';
 
 const HORIZONS = [
